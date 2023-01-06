63fc66e0255d8ead4698c61871c307183d2bd9e2
Diego Danner <diegodanner@gmail.com>
2023-01-06 08:53:25 -0300

    new update
    new update

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,10 +1,10 @@
-class Passaporte {
+class Atleta {
   constructor(nome, sobrenome){
       this.nome = nome
       this.sobrenome = sobrenome
   }
   obterNome(){
-      return this.nome.toLowerCase()
+      return this.nome.toUpperCase()
   }
   obterSobrenome(){
       return this.sobrenome.toUpperCase()
@@ -12,11 +12,14 @@ class Passaporte {
   obterNomeCompleto(){
       return this.nome +" "+ this.sobrenome
   }
-  obterIniciais(){
-      return this.nome[0] +"."+ this.sobrenome[0] + "." 
+  retornaListaNotas(){
+    let atletas = notas.map(function(notas){
+        console.log(notas)
+        return atletas.notas
+    })
   }
   obterEhNomeValido(){
-      if (
+      /*if (
           this.nome.length > 0 &&
           this.sobrenome.length > 0 &&
           this.sobrenome.endsWith(".") === false
@@ -25,18 +28,34 @@ class Passaporte {
       } else {
           return "Não";
       }
-  }
+  }*/
 }
 
+ atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+     },
+     {
+       nome: "Fernando Puntel",
+       notas:  [8, 10, 10, 7, 9.33]
+     },
+     {
+       nome: "Daiane Jelinsky",
+       notas: [7, 10, 9.5, 9.5, 8]
+     },
+     {
+       nome: "Bruno Castro",
+       notas: [10, 10, 10, 9, 9.5]
+     }
+       ]
 // Uso da classe
-let mario = new Passaporte("Mario", "Borges");
+let mario = new Atleta("Mario", "Borges");
 console.log(mario.obterNome());
 console.log(mario.obterSobrenome());
 console.log(mario.obterNomeCompleto());
-console.log(mario.obterIniciais());
+console.log(mario.retornaListaNotas());
 console.log(mario.obterEhNomeValido());
-
-console.log("---");
 
 
 let nome = document.querySelector("#dv-nome");
@@ -49,14 +68,14 @@ let dvResposta4 = document.querySelector("#dv-resposta-4");
 let dvResposta5 = document.querySelector("#dv-resposta-5");
 
 function formatar() {
-    let devPassaporte = new Passaporte(nome.value, sobrenome.value);
+    let devAtleta = new Atleta (nome.value, sobrenome.value);
 
-    dvResposta1.textContent = devPassaporte.obterNome();
-    dvResposta2.textContent = devPassaporte.obterSobrenome();
-    dvResposta3.textContent = devPassaporte.obterNomeCompleto();
-    dvResposta4.textContent = devPassaporte.obterIniciais();
-    dvResposta5.textContent = devPassaporte.obterEhNomeValido();
+    dvResposta1.textContent = devAtleta.obterNome();
+    dvResposta2.textContent = devAtleta.obterSobrenome();
+    dvResposta3.textContent = devAtleta.obterNomeCompleto();
+    dvResposta4.textContent = devAtleta.retornaListaNotas();
+    dvResposta5.textContent = devAtleta.obterEhNomeValido();
 }
 
-nome.addEventListener("keyup", formatar);
+nome.addEventListener(`keyup`, formatar);
 sobrenome.addEventListener("keyup", formatar);
